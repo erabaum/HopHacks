c7fa3f2aebb612b635c879c82cdb0ecf4f17e6b3
Daniel Evans <danevans@terpmail.umd.edu>
2016-10-22 20:42:47 -0400

    Add Track Without Proper URL

--- addTrack.js
+++ addTrack.js
@@ -1,3 +1,4 @@
+
 var track;
 var res;
 
@@ -6,32 +7,43 @@ var searchTracks = function () {
   var group_name = document.getElementById('group_name').value;
   // alert("Hello World!");
   $.ajax({
-    method: "GET",
-    url: 'https://api.spotify.com/v1/search?type=track&q=' + query +'',
-    success: function(response) { 
-      res = response;
-      if (response.tracks.items.length) {
-
-        track = response.tracks.items[0];
-
-        app.post('/add', function(req, res) {
-          var song = {
-            room: group_name, description: track,
-          };
-          db.insert(song, function(err,song) {
-            if (err) res.send(err);
-            res.json(song);
-          });
-          res.redirect('/');
-        });
-      });
-
-    }
-  },
+      method: "GET",
+      url: 'https://api.spotify.com/v1/search?type=track&q=' + query +'',
+      success: function(response) {	
+      	res = response;
+        if (response.tracks.items.length) {
+        
+          track = response.tracks.items[0];
+          document.write(track.id);
+          document.write(group_name);
+      	}
+    },
     error: function(response) {
-      console.log(response);
+    	console.log(response);
     }
-});
+  });
+
+  var data = {
+    'room' : group_name
+    'description' : track.id
+  }
+
+  var url = Object.keys(data).map(function(k) {
+    return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+  }).join('&')
+
+  fetch('http://localhost:8081/songs', { //Change this URL to the one with songs.db
+    method: "POST",
+    body: url
+  }).then(res => {
+    if (res.ok) {
+    res.json().then(data => console.log(data))
+    } else {
+      console.log(res)
+    }
+  })
+
+
 }
 
 
@@ -41,4 +53,3 @@ var searchTracks = function () {
 //   alert("1");
 //   searchTracks(document.getElementById('query').value);
 // }, false);
-
