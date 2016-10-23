902d2be9d06ccc2db10d81a04a430a78f52ef5ed
Ubuntu <ubuntu@ip-172-31-53-66.ec2.internal>
2016-10-23 06:20:55 +0000

    Got expres to recognize static files

---
+++ js/addTrack.js
@@ -0,0 +1,49 @@
+var track;
+var res;
+var groupName;
+
+var getGroupName = function () {
+  groupName = document.getElementById('groupName').value;
+} 
+
+var searchTracks = function () {
+  var query = document.getElementById('query').value;
+  var group_name = document.getElementById('group_name').value;
+
+  // Query for the data first track related to that name
+  $.ajax({
+    method: "GET",
+    url: 'https://api.spotify.com/v1/search?type=track&q=' + query +'',
+    success: function(response) {	
+      res = response;
+      if (response.tracks.items.length) {
+        track = response.tracks.items[0];
+      }
+    },
+    error: function(response) {
+      console.log(response);
+    }
+  });
+
+  // Set the data just queried
+  var data = {
+    'room' : groupName
+    'description' : track.id
+  }
+
+  var url = Object.keys(data).map(function(k) {
+    return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+  }).join('&')
+
+  fetch('ec2-52-207-254-231.compute-1.amazonaws.com:8081/songs.db', { //Change this URL to the one with songs.db
+    method: "POST",
+    body: url
+  }).then(res => {
+    if (res.ok) {
+      res.json().then(data => console.log(data))
+    } else {
+      console.log(res)
+    }
+  })
+
+}
